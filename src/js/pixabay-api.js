686df44df3237ba638c0674c1c155e403783ff33
Misha908989 <mishazaharevich71@gmail.com>
2026-02-03 17:18:30 +0200

    dfgf

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,14 +3,17 @@ import axios from 'axios';
 const API_KEY = '54476211-a145d35bc2dca18e6906896b1';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page = 1) {
   const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page: page,
+    per_page: 15,
   };
 
-  return axios.get(BASE_URL, { params }).then(response => response.data);
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
 }
